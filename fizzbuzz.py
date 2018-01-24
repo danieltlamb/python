ffff3eb05c90a761cc7097bdc6a841261bb659fc
Daniel Lamb <daniel.lamb@cybergrx.com>
2018-01-23 18:22:23 -0700

    Improved

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 
-
 for i in range(1, 101):
-        if i%3 != 0:
-		if i%5 != 0:
-               		 print i
-	if i%3 == 0: 
-		if i%5 == 0:
-			print "FizzBuzz"
-	if i%3 == 0:
-		print "Fizz"
-	if i%5 == 0:
-		print "Buzz"
+    if i%3 == 0 and i%5 == 0:
+        print "FizzBuzz"
+    elif i%3 == 0 and i%5 != 0:
+        print "Fizz"
+    elif i%5 == 0 and i%3 != 0:
+        print "Buzz"
+    elif i%5 != 0 and i%5 != 0:
+	print i
